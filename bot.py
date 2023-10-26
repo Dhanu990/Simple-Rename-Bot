ff747dc277e0a374527773f9745523aa408780f3
Dhanu990 <130265533+Dhanu990@users.noreply.github.com>
2023-10-26 13:19:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 from pyrogram import Client
-from pyrogram import types
-from pyrogram.raw import functions
 from config import *
 import os
 
@@ -10,7 +8,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "simple-renamer",
+            name="simple-renamer",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -18,36 +16,15 @@ class Bot(Client):
             plugins={"root": "main"},
             sleep_threshold=10,
         )
-
     async def start(self):
         await super().start()
-        me = await self.get_me()
+        me = await self.get_me()      
         print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
-
+       
     async def stop(self, *args):
-        await super().stop()
-        print("Bot Restarting........")
+       await super().stop()      
+       print("Bot Restarting........")
 
-    async def on_upload_progress(self, current, total):
-        # Handle progress updates for large file uploads here
-        # For example, you can log the progress or send updates to the user.
-
-    async def upload_large_file(self, chat_id, file_path, file_name):
-        try:
-            result = await self.send(
-                functions.messages.UploadMedia(
-                    peer=await self.resolve_peer(chat_id),
-                    media=types.InputMediaUploadedDocument(
-                        file=await self.upload_file(file_path),
-                        force_document=True,
-                        attributes=[types.DocumentAttributeFilename(file_name)],
-                    ),
-                ),
-                file_name=file_name,
-            )
-            return result
-        except Exception as e:
-            print(f"Error uploading the file: {e}")
 
 bot = Bot()
 bot.run()
